6f8b9e5e00d71100df270f14de47ad376f9bce71
ohkubo kohei <kuboon@trick-with.net>
2020-02-17 02:21:22 +0000

    canvas sample

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -4,6 +4,8 @@ extern crate cfg_if;
 extern crate wasm_bindgen;
 extern crate web_sys;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -27,25 +29,52 @@ cfg_if! {
     }
 }
 
-// Called by our JS entry point to run the example
 #[wasm_bindgen]
-pub fn run() -> Result<(), JsValue> {
-    // If the `console_error_panic_hook` feature is enabled this will set a panic hook, otherwise
-    // it will do nothing.
-    set_panic_hook();
+extern "C" {
+    fn setInterval(closure: &Closure<dyn FnMut()>, time: u32) -> i32;
+    fn clearInterval(id: i32);
 
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+const W: u32 = 540;
+const N: u32 = 100;
+#[wasm_bindgen]
+pub fn init() -> Result<(), JsValue> {
+    set_panic_hook();
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust, WebAssembly, and Parcel!");
-
-    body.append_child(&val)?;
-
+    let canvas: web_sys::HtmlCanvasElement = document.create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    canvas.set_id("canvas");
+    canvas.set_height(W);
+    canvas.set_width(W);
+    body.append_child(&canvas)?;
     Ok(())
 }
 
+const R: f64 = std::f64::consts::PI * 2.0 / N as f64;
+static FRAME_COUNT: AtomicUsize = AtomicUsize::new(0);
+static mut x: f64 = 0.0;
+static mut y: f64 = 0.0;
+#[wasm_bindgen]
+pub fn draw(ctx: &web_sys::CanvasRenderingContext2d) {
+    let count = FRAME_COUNT.fetch_add(1, Ordering::SeqCst);
+    let t: f64 = count as f64 / 10.0;
+    ctx.clear_rect(0.0, 0.0, W as f64, W as f64);
+    for i in 0..N {
+        for j in 0..N {
+            let ii = i as f64;
+            ctx.set_fill_style(&JsValue::from_str(&format!("rgb({},{},{})",i,j,255)));
+            unsafe {
+                let u = (ii + y).sin() + (R * ii + x).sin();
+                let v = (ii + y).cos() + (R * ii + x).cos();
+                x = u + t;
+                y = v;
+                ctx.fill_rect(u * (N/2) as f64 + (W/2) as f64, y * (N/2) as f64 + (W/2) as f64, 2.0, 2.0);
+            }
+        }
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 import module from '../crate/Cargo.toml'
-module.run();
+module.init();
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+setInterval(()=>{module.draw(ctx)}, 0);
