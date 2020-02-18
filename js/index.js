4a2a0a4c82bfa3964104f791ff083df55497cca6
ohkubo kohei <kuboon@trick-with.net>
2020-02-18 13:05:26 +0000

    gh-pages

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import module from '../Cargo.toml'
-module.init();
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-let count = 0;
-setInterval(()=>{
-  const ret = module.draw(ctx)
-}, 1);
